f12b28e65d9992c72935af81227e7de363d5d8e2
pryfmse <nst-dmv@yandex.ru>
2025-07-11 13:55:57 +0300

    fix sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,25 +1,51 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BurgerBtn from '../burger_button/BurgerBtn'
 import './sidebar.scss'
 
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
+
+    const handleClose = () => {
+        setIsClosing(true);
+    };
+
+    useEffect(() => {
+        if (isClosing) {
+            const timer = setTimeout(() => {
+                setIsOpen(false);
+                setIsClosing(false);
+            }, 300); 
+            return () => clearTimeout(timer);
+        }
+    }, [isClosing]);
+
+
     return(
-        <div className={isOpen ? 'sidebar-window' : ''}>
+        <div>
             <div className='fixed p-4 z-1000'>
-                <BurgerBtn onClick={() => setIsOpen(prev => !prev)} isOpen={isOpen} />
+                <BurgerBtn
+                    onClick={() => {
+                        if (isOpen) {
+                        handleClose(); 
+                        } else {
+                        setIsOpen(true); // 
+                        }
+                    }}
+                    isOpen={isOpen}
+                />
             </div>
             {isOpen ? 
             <>
-                <div className='sidebar'>
+                <div className={`sidebar ${isClosing ? 'sidebarClose' : ''}`}>
                     <div className='h-[8vh] shadow-lg w-full'></div>
                     <div>
                         
                     </div>
                 </div>
-                <div className='sidebar-close' onClick={() => setIsOpen(prev => !prev)}></div>
+                <div className={`shadow ${isClosing ? 'shadowClose' : ''}`} onClick={handleClose}></div>
             </>
             : null  }
         </div>
