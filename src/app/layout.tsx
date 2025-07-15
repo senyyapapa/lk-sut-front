61eed93607f5be6c4bb369970c4b947fcd6f853b
pryfmse <nst-dmv@yandex.ru>
2025-07-15 20:07:27 +0300

    fixed HydrationWarning related to theme changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ru">
+    <html lang="ru" suppressHydrationWarning={true}>
       <head>
         <ThemeScript />
       </head>
